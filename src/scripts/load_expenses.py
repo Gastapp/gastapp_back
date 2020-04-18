edb20e2a5443a7fbe236b08e363303ffc7f5a05a
Mauro <reekremag@gmail.com>
2020-04-18 10:13:21 -0300

    changes to load_expenses script

--- src/scripts/load_expenses.py
+++ src/scripts/load_expenses.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
+
 conn = MongoClient(host='127.0.0.1', port=27017)
 conn = MongoClient()
 
@@ -7,12 +8,14 @@ db = conn.gastapp
 collection = db.expenses
 
 expenses = [
-    {"id_user": "1", "amount": "450", "category": "food"},
-    {"id_user": "1", "amount": "1500", "category": "clothes"},
-    {"id_user": "2", "amount": "5200", "category": "home appliances"},
-    {"id_user": "2", "amount": "300", "category": "food"},
-    {"id_user": "2", "amount": "235", "category": "clothes"},
-    {"id_user": "3", "amount": "1500", "category": "clothes"},
+    {"id_user": "1", "amount": "450", "category": "food", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "235", "category": "clothes", "date": "10/03/2020"},
+    {"id_user": "1", "amount": "1500", "category": "clothes", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "5200", "category": "home appliances", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "300", "category": "food", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "5200", "category": "home appliances", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "300", "category": "food", "date": "17/04/2020"},
+    {"id_user": "3", "amount": "1500", "category": "clothes", "date": "17/04/2020"},
 
 ]
 
