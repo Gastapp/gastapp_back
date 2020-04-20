90110334615adcf6987647be56c12c02e8b977eb
Mauro Bailon <reekremag@gmail.com>
2020-04-20 17:50:02 -0300

    change script

--- src/scripts/load_expenses.py
+++ src/scripts/load_expenses.py
@@ -9,13 +9,13 @@ collection = db.expenses
 
 expenses = [
     {"id_user": "1", "amount": "450", "category": "food", "date": "17/04/2020"},
-    {"id_user": "2", "amount": "235", "category": "clothes", "date": "10/03/2020"},
+    {"id_user": "1", "amount": "235", "category": "clothes", "date": "10/03/2020"},
     {"id_user": "1", "amount": "1500", "category": "clothes", "date": "17/04/2020"},
     {"id_user": "2", "amount": "5200", "category": "home appliances", "date": "17/04/2020"},
     {"id_user": "2", "amount": "300", "category": "food", "date": "17/04/2020"},
     {"id_user": "2", "amount": "5200", "category": "home appliances", "date": "17/04/2020"},
     {"id_user": "2", "amount": "300", "category": "food", "date": "17/04/2020"},
-    {"id_user": "3", "amount": "1500", "category": "clothes", "date": "17/04/2020"},
+    {"id_user": "2", "amount": "1500", "category": "clothes", "date": "17/04/2020"},
 
 ]
 
