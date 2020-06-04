ea243a51cb57fa61a2ef14aa7e3c2f339f491389
Mauro <reekremag@gmail.com>
2020-06-04 18:27:50 -0300

    corrijo persistencia del tipo de cuenta en ingresos y gastos

--- src/model/expense.py
+++ src/model/expense.py
@@ -1,6 +1,6 @@
 class Expense:
 
-    def __init__(self, user_email, amount, category, date, account, description=""):
+    def __init__(self, user_email, amount, category, account, date, description=""):
         self.user_email = user_email
         self.amount = amount
         self.category = category
